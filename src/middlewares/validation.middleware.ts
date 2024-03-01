6b61388ec70c51e5892152baaa30c68402c00ccc
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2024-02-29 23:44:46 -0300

    fix: improving error message pattern

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -2,14 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { Schema } from "zod";
 import { CustomError } from "../models";
 
-const extractErrorMessage = (type: "body" | "params" | "query", err: any) => {
+const extractErrorMessage = (err: any) => {
   const { path, received, message, expected } = err;
 
-  const paramMessage = type === "query" ? "Na query" : type === "params" ? "Nos params" : "No body";
-
-  return `${paramMessage}: O campo '${path}' recebeu '${received}'. Erro:'${message}'.${
-    expected ? ` Esperado: '${expected}'` : ""
-  }`;
+  return `O campo '${path}' recebeu '${received}'.
+  Erro:'${message}'.${expected ? ` Esperado: '${expected}'` : ""}`;
 };
 
 const validate = (schema: Schema, type: "body" | "params" | "query") => {
@@ -22,7 +19,7 @@ const validate = (schema: Schema, type: "body" | "params" | "query") => {
       if (error.errors) {
         const errArray = error.errors;
         for (const err of errArray) {
-          const errMessage = extractErrorMessage(type, err);
+          const errMessage = extractErrorMessage(err);
 
           next(new CustomError(errMessage, 400));
         }
