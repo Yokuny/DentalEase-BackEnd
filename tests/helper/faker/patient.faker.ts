301662d28c90962c7f560dcd20b02afa9c47aa8a
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2024-04-07 22:05:19 -0300

    build: fix imports to build process works

--- tests/helper/faker/patient.faker.ts
+++ tests/helper/faker/patient.faker.ts
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker";
 import RandExp from "randexp";
 import { cpfRegex, rgRegex, phoneRegex, cepRegex } from "../../../src/helpers/regex.helper";
-import type { NewPatient, Anamnesis, Intraoral, ClinicPatient } from "../../../src/models";
+import type { NewPatient } from "../../../src/models";
 
 export function createRandomPatient(): NewPatient {
   return {
@@ -9,7 +9,7 @@ export function createRandomPatient(): NewPatient {
     email: faker.internet.email(),
     cpf: new RandExp(cpfRegex).gen(),
     rg: new RandExp(rgRegex).gen(),
-    birthdate: faker.date.past(),
+    birthdate: faker.date.past().toISOString(),
     sex: faker.helpers.arrayElement(["M", "F"]),
     phone: new RandExp(phoneRegex).gen(),
     cep: new RandExp(cepRegex).gen(),
